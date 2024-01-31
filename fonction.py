0819e566c8cfdae20681a31865c3145d185c0ca6
lilcdx <113095135+lilcdx@users.noreply.github.com>
2024-01-31 16:12:07 +0100

    j,

--- fonction.py
+++ fonction.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
+import pyarrow.compute as pc
 
 def dataframe_to_table(df):
     table = pa.Table.from_pandas(df)
@@ -36,3 +37,24 @@ def get_column(table, col_name):
         print(f"La colonne {col_name} n'existe pas dans la table.")
         return None
     
+def compute_statistics(table, col_name):
+    col = table[col_name]
+    count_result = pc.count(col)
+    count_distinct_result = pc.count_distinct(col)
+    sum_result = pc.sum(col)
+    min_result = pc.min(col)
+    max_result = pc.max(col)
+
+    # Obtenir les valeurs des résultats
+    count_value = count_result.as_py()
+    count_distinct_value = count_distinct_result.as_py()
+    sum_value = sum_result.as_py()
+    min_value = min_result.as_py()
+    max_value = max_result.as_py()
+
+    # Afficher les résultats
+    print(f"Count: {count_value}")
+    print(f"Count Distinct: {count_distinct_value}")
+    print(f"Sum: {sum_value}")
+    print(f"Min: {min_value}")
+    print(f"Max: {max_value}")
--- parquet.py
+++ parquet.py
@@ -2,3 +2,4 @@ from fonction import *
 
 table_academie = parquet_to_table("academie.parquet")
 print(get_column(table_academie, "dep"))
+print(compute_statistics())
